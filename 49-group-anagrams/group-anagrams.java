f19b5a6229f4711c75bfcc18d09e2d0761d504e3
Bhanuprakash Reddy <106879319+Bhanu0301@users.noreply.github.com>
2025-12-19 15:23:06 +0530

    Time: 7 ms (72.44%) | Memory: 49.5 MB (55.22%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //eat -> eat, tea, ate
+        //tan -> tan, nat
+        //bat -> bat
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+            if(!map.containsKey(sortedStr)){
+                map.put(sortedStr, new ArrayList<>());     
+            }
+            map.get(sortedStr).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
