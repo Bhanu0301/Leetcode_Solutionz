a085211a54651c79a759d31a9e45c81a2136d06d
Bhanuprakash Reddy <106879319+Bhanu0301@users.noreply.github.com>
2025-12-29 11:11:17 +0530

    Time: 5 ms (60.78%) | Memory: 83 MB (31.28%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,11 +1,14 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        //if we analyze this problem is similar to LL cycle 2 where we need to find the node where cycle starts
         int slow = nums[0];
         int fast = nums[0];
+        //there is cycle for sure.
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
         }while(slow!=fast);
+        //now the met at a point , next step is to find their start point.
         fast = nums[0];
         while(slow!=fast){
             slow = nums[slow];
