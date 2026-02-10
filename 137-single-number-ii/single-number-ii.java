322a0146117af8ddbfd4b6b5becd6450eae7cf3e
Bhanuprakash Reddy <106879319+Bhanu0301@users.noreply.github.com>
2026-02-10 09:15:27 +0530

    Time: 1 ms (93.72%) | Memory: 45.1 MB (85.97%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,18 +1,36 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int res = 0;
-        for(int i = 0; i<32; i++){
-            int sum = 0;
-            for(int num : nums){
-                //check if bit 'i' is set
-                if(((num>>i)&1)==1){
-                    sum++;
-                }
-            }
-            if(sum%3!=0){
-                res = (res|(1<<i));
-            }
+        int ones = 0;
+        int twos = 0;
+        //idea -> 
+        /* 
+        1st time -> ones
+        2nd time -> twos
+        3rd time -> remove
+
+        0^a = a, a^a = 0 --> this toggleness is used
+        if(not present in ones, we can add (0^a=a))
+        else(remove from ones and add in twos)(a^a=0)->ones
+            (0^a)->twos
+        */
+        for(int num : nums){
+            ones = (ones ^ num) & (~twos); 
+            twos = (twos ^ num) & (~ones); //in twos if not in ones
         }
-        return res;
+        return ones;
+        // int res = 0;
+        // for(int i = 0; i<32; i++){
+        //     int sum = 0;
+        //     for(int num : nums){
+        //         //check if bit 'i' is set
+        //         if(((num>>i)&1)==1){
+        //             sum++;
+        //         }
+        //     }
+        //     if(sum%3!=0){
+        //         res = (res|(1<<i));
+        //     }
+        // }
+        // return res;
     }
 }
