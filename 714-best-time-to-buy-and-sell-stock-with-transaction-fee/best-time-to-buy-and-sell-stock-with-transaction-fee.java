5922084bb5c9a23bc9551afe65c98541a8a76a9c
Bhanuprakash Reddy <106879319+Bhanu0301@users.noreply.github.com>
2026-02-16 10:06:09 +0530

    Time: 16 ms (50.93%) | Memory: 70.8 MB (37.26%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,24 +1,36 @@
 class Solution {
-    private int getProfit(int[] prices, int fee, int n, int ind, int buy, int[][] dp){
-        if(ind==n)return 0;
-        if(dp[ind][buy]!=-1)return dp[ind][buy];
-        int profit = 0;
-        if(buy == 0){
-            profit = Math.max(0+getProfit(prices, fee,n,  ind+1, 0, dp),
-                -prices[ind]+getProfit(prices, fee, n, ind+1, 1, dp));
-        }
-        else{
-            profit = Math.max(0+getProfit(prices, fee,n,  ind+1, 1, dp),
-                prices[ind]+getProfit(prices, fee, n, ind+1, 0, dp)-fee);
-        }
-        dp[ind][buy] = profit;
-        return dp[ind][buy];
-    }
+    // private int getProfit(int[] prices, int fee, int n, int ind, int buy, int[][] dp){
+    //     if(ind==n)return 0;
+    //     if(dp[ind][buy]!=-1)return dp[ind][buy];
+    //     int profit = 0;
+    //     if(buy == 0){
+    //         profit = Math.max(0+getProfit(prices, fee,n,  ind+1, 0, dp),
+    //             -prices[ind]+getProfit(prices, fee, n, ind+1, 1, dp));
+    //     }
+    //     else{
+    //         profit = Math.max(0+getProfit(prices, fee,n,  ind+1, 1, dp),
+    //             prices[ind]+getProfit(prices, fee, n, ind+1, 0, dp)-fee);
+    //     }
+    //     dp[ind][buy] = profit;
+    //     return dp[ind][buy];
+    // }
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
-        for(int[] arr : dp)
-            Arrays.fill(arr, -1);
-        return (getProfit(prices, fee, n, 0, 0, dp));
+        int[][] dp = new int[n+1][2];
+        for(int ind=n-1; ind>=0; ind--){
+            for(int buy =0;buy<=1;buy++){
+                int profit = 0;
+                if(buy == 0){
+                    profit = Math.max(0+dp[ind+1][0],
+                        -prices[ind]+dp[ind+1][1]);
+                }
+                else{
+                    profit = Math.max(0+dp[ind+1][1],
+                        prices[ind]+dp[ind+1][0]-fee);
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][0];
     }
 }
