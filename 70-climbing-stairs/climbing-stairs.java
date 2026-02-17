1b936bf8a257b1d20fe18ef65ea364d1e2d1976e
Bhanuprakash Reddy <106879319+Bhanu0301@users.noreply.github.com>
2026-02-17 11:22:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (54.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,14 +1,5 @@
 class Solution {
-    private int climbStairsHelp(int currStep, int[] dp){
-        if(currStep == 0|| currStep == 1)return 1;
-        if(dp[currStep]!=-1)return dp[currStep];
-        int oneStep = climbStairsHelp(currStep-1, dp);
-        int twoSteps = climbStairsHelp(currStep-2, dp);
-
-        return dp[currStep] = oneStep + twoSteps;
-    }
     public int climbStairs(int n) {
-        int[] dp = new int[n+2];
         int prev2=1;
         int prev1=1;
         int curr=0;
