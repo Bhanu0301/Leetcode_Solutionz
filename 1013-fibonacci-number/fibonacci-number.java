dfbc50dc97e6bd0b4e414987dd6016f314e2d48e
Bhanuprakash Reddy <106879319+Bhanu0301@users.noreply.github.com>
2026-02-17 10:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (85.98%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,12 +1,12 @@
 class Solution {
-    private int findFib(int n, int[] dp){
-        if(n==0||n==1)return n;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=findFib(n-1,dp)+findFib(n-2,dp);
-    }
     public int fib(int n) {
+        if(n==0||n==1)return n;
         int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return findFib(n, dp);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
