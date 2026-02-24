cc5d1398817ae709d2afdaba75d9197c6152cc18
Bhanuprakash Reddy <106879319+Bhanu0301@users.noreply.github.com>
2026-02-24 10:56:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (66.21%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,26 +1,29 @@
 class Solution {
-    private int f(int r, int c, int m, int n, int[][] dp){
-        if(r==m-1 && c==n-1){
-            return 1;
-        }
-        if(r>m-1 || c>n-1)return 0;
-        if(dp[r][c]!=-1)return dp[r][c];
-        int right = f(r, c+1, m, n, dp);
-        int down = f(r+1, c, m, n, dp);
-        return dp[r][c] = right + down;
-    }
+    // private int f(int r, int c, int m, int n, int[][] dp){
+    //     if(r==m-1 && c==n-1){
+    //         return 1;
+    //     }
+    //     if(r>m-1 || c>n-1)return 0;
+    //     if(dp[r][c]!=-1)return dp[r][c];
+    //     int right = f(r, c+1, m, n, dp);
+    //     int down = f(r+1, c, m, n, dp);
+    //     return dp[r][c] = right + down;
+    // }
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m+1][n+1];
+        int prev[] = new int[n+1];
         for(int r = m-1; r>=0; r--){
+            int cur[] = new int[n+1];
             for(int c = n-1; c>=0; c--){
-                if(r==m-1 && c==n-1)dp[r][c] = 1;
+                if(r==m-1 && c==n-1)cur[c] = 1;
                 else{
-                    int right = c<0? 0 : dp[r][c+1];
-                    int down = r<0? 0 : dp[r+1][c];
-                    dp[r][c] = right + down;
+                    int right = c<0? 0 :cur[c+1];
+                    int down = r<0? 0 : prev[c];
+                    cur[c] = right + down;
                 }
             }
+            prev = cur;
         }
-        return dp[0][0];
+        return prev[0];
     }
 }
